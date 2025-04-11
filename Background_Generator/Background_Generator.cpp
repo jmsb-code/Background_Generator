c4c277bd07707dd13018131a215044db52a7ead2
James Broderick <pjimmythedude@gmail.com>
2025-04-10 20:15:48 -0400

    Add project files.

---
+++ Background_Generator/Background_Generator.cpp
@@ -0,0 +1,12 @@
+﻿// Background_Generator.cpp : Defines the entry point for the application.
+//
+
+#include "Background_Generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
