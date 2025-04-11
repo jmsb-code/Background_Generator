7ffd93827c4e0734de71c2dcc932551440295cbb
James Broderick <pjimmythedude@gmail.com>
2025-04-10 21:14:56 -0400

    cImg and json parser linked to cmake

--- Background_Generator/Background_Generator.cpp
+++ Background_Generator/Background_Generator.cpp
@@ -3,6 +3,11 @@
 
 #include "Background_Generator.h"
 
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+
 using namespace std;
 
 int main()
--- Background_Generator/Background_Generator.h
+++ Background_Generator/Background_Generator.h
@@ -5,4 +5,16 @@
 
 #include <iostream>
 
+
+struct Background {
+	int tileW;
+	int tileH;
+	int** tiles; //neighbor values for tiles
+	std::string tsfname;
+	uint8_t ssID;
+	bool texturesSet = false;
+
+	Background(const char* filename);
+};
+
 // TODO: Reference additional headers your program requires here.
